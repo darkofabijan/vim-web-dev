c5941eb0327db5905333e4640c6c416ed34f70b0
Aleksandar Diklic <rastasheep3@gmail.com>
2013-04-05 19:54:03 +0200

    added multibyte support

--- install-vim.sh
+++ install-vim.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y mercurial
 cd /tmp
 hg clone https://vim.googlecode.com/hg/ vim
 cd vim
-./configure --enable-rubyinterp --enable-gui=no
+./configure --enable-rubyinterp --enable-multibyte --enable-gui=no
 make
 sudo make install
 cd /tmp
